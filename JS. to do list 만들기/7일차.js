780741e1fc4984ffc62aa017737b04fe3bbd60e9
최도은 <choidoeun0102@gmail.com>
2023-07-07 16:34:44 +0900

    계산기 만들기

---
+++ JS. to do list 만들기/7일차.js
@@ -0,0 +1,3 @@
+let input= document.getElementById('input');
+let todolist = document.getElementById('todolist');
+let list = document.getElementById('list');
---
+++ JS. 계산기 만들기/script.js
@@ -0,0 +1,25 @@
+var arr = [];
+function add(char) {
+    var display = document.getElementById('display');
+    display.value = display.value + char;
+}
+function calculate() {
+    var display = document.getElementById('display');
+    var result = eval(display.value);
+    document.getElementById('result').value = result;
+    arr.push(display.value + '=' + result);
+    
+}
+function reset() {
+    document.getElementById('display').value = "";
+    document.getElementById('result').value = "";
+}
+function record() {
+    const ul = document.createElement('ul');
+    arr.forEach(item => {
+      const liElement = document.createElement('li');
+      liElement.textContent = item;
+      ul.appendChild(liElement);
+    });
+    document.body.appendChild(ul);
+  }
